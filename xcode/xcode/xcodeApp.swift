02841c688c49c760f39d4be1de70c92ef4bd9011
Maksim Stashkevich <makstashkevich@gmail.com>
2026-01-11 19:56:06 +0300

    Added: Toggle, NextJs examples, Swift xcode test app, react hooks & more stylex fixes

---
+++ xcode/xcode/xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeApp.swift
+//  xcode
+//
+//  Created by Maksim Stashkevich on 09.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xcode/xcodeTests/xcodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeTests.swift
+//  xcodeTests
+//
+//  Created by Maksim Stashkevich on 09.01.2026.
+//
+
+import Testing
+@testable import xcode
+
+struct xcodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
